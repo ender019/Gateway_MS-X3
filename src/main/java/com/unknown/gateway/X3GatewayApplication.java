782f8e2f9d056ff0bb2a63e7aa10a7520978dd19
ender019 <ender-fox@mail.ru>
2025-07-06 11:59:54 +0300

    Init commit

---
+++ src/main/java/com/unknown/gateway/X3GatewayApplication.java
@@ -0,0 +1,13 @@
+package com.unknown.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class X3GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(X3GatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/unknown/gateway/X3GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unknown.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class X3GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
