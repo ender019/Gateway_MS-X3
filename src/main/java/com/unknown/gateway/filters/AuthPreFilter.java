f719463a23bcf1e1cce9b2ee8ea8deb417f0b6f0
ender019 <ender-fox@mail.ru>
2025-07-29 17:11:59 +0300

    Добавление фильтра для обработки внутренних сервисов.

--- src/main/java/com/unknown/gateway/filters/AuthPreFilter.java
+++ src/main/java/com/unknown/gateway/filters/AuthPreFilter.java
@@ -3,6 +3,7 @@ package com.unknown.gateway.filters;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.stereotype.Component;
@@ -12,12 +13,12 @@ import org.springframework.stereotype.Component;
 public class AuthPreFilter{
 
     static {
-        log.info("AuthPreFilter");
+        log.debug("AuthPreFilter");
     }
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        log.info("securityFilterChain executed");
+        log.trace("securityFilterChain executed");
         http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
@@ -26,8 +27,9 @@ public class AuthPreFilter{
         return http.authorizeExchange(c -> c
                         .pathMatchers("/error").permitAll()
                         .pathMatchers("/auth/**").permitAll()
+                        .pathMatchers("/service/**").permitAll()
                         .pathMatchers("/auth/logout").authenticated()
                         .anyExchange().authenticated()
-                ).build();
+                ).addFilterAfter(new ServicePreFilter(), SecurityWebFiltersOrder.AUTHORIZATION).build();
     }
 }
